0ee828576edfd198c2784331f195782b1df89b50
Hoan Nguyen <user.name>
2024-01-28 05:52:59 +0700

    update ux

--- src/app/gifs/gif-card/gif-card.component.ts
+++ src/app/gifs/gif-card/gif-card.component.ts
@@ -14,6 +14,7 @@ export class GifCardComponent {
   isOpen = false;
   viewCount = 0;
   copyStatus = 0;
+  onHover = false;
   constructor(private gifsService: GifsService) {}
 
   toggle(id: string) {
@@ -31,5 +32,13 @@ export class GifCardComponent {
     setTimeout(() => {
       this.copyStatus = 0;
     }, 1000);
+  }
+
+  hover() {
+    this.onHover = true;
+  }
+
+  leave() {
+    this.onHover = false;
   }
 }
